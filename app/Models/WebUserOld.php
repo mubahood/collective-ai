88d628168d94064e0f5f6613b97e05093579674b
Muhindo Mubaraka <mubahood360@gmail.com>
2025-01-22 15:13:51 +0300

    Remove WebUser model and related cache files; add WebUserOld model

--- app/Models/WebUserOld.php
+++ app/Models/WebUserOld.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class WebUser extends Model
+class WebUserOld extends Model
 {
     use HasFactory;
 }
